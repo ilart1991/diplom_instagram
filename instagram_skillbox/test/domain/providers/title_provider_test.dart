9dc3d0188b5a6b0e669c743159a07166ab947a61
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2023-01-08 10:32:33 +0300

    add some tests

--- instagram_skillbox/test/domain/providers/title_provider_test.dart
+++ instagram_skillbox/test/domain/providers/title_provider_test.dart
@@ -1,15 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:instagram_skillbox/domain/providers/title_provider.dart';
+import 'package:instagram_skillbox/main.dart';
+import 'package:instagram_skillbox/presentation/pages/home_page.dart';
+import 'package:instagram_skillbox/presentation/pages/login_page.dart';
+
+import 'package:localization/localization.dart';
 
 void main() {
-  test('title provider ...', () {
+  testWidgets('title provider ...', (tester) async {
+    runApp(const ProviderScope(child: MyApp()));
+
+    await tester.pumpWidget(const MaterialApp(
+      home: ProviderScope(child: LoginPage(title: "FlutterGram")),
+    ));
+    expect(find.text("FlutterGram"), findsOneWidget);
+    expect(find.text("E-mail"), findsWidgets);
+    expect(find.text("password".i18n()), findsWidgets);
     final container = ProviderContainer();
 
     var titleGallery = container.read(titleProvider);
-    expect(titleGallery, "Галерея");
+    expect(titleGallery, "gallery".i18n());
     container.read(titleProvider.notifier).profileTitle();
     var titleProfile = container.read(titleProvider);
-    expect(titleProfile, "Профиль");
+    expect(titleProfile, "profile".i18n());
   });
 }
