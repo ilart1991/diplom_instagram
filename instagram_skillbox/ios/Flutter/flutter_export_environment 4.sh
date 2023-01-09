492130ebf6faab5824ffe5f41a9a52776cb2af0f
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2023-01-09 09:27:39 +0300

    ok

---
+++ instagram_skillbox/ios/Flutter/flutter_export_environment 4.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ilart1991/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ilart1991/Documents/GitHub/diplom_instagram/instagram_skillbox"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/ilart1991/Documents/GitHub/diplom_instagram/instagram_skillbox/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/ilart1991/Documents/GitHub/diplom_instagram/instagram_skillbox/.dart_tool/package_config.json"
